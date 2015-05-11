11025725b84d35d0f9b835e52eb2d5cda101b2df
Daniel <nieltg@users.noreply.github.com>
2015-05-11 23:01:11 +0700

    Use patches for Material-themed sounds
    This commit fixes 'None' phone & notification ringtone after full data
    wipe. Actually, I don't know what's default Material sounds in L-based
    phones like Android One, so I just choose my favorites.
    
    This reverts commit addb65b461a4152f089b551e1c7fa99dfda5a924.

--- full_codina.mk
+++ full_codina.mk
@@ -1,33 +1,10 @@
-# Full base telephony, modified.
-
-PRODUCT_PACKAGES := \
-    libfwdlockengine \
-    WAPPushManager
-
-PRODUCT_PROPERTY_OVERRIDES := \
-    ro.com.android.dateformat=MM-dd-yyyy
-
-PRODUCT_LOCALES := en_US
-PRODUCT_AAPT_CONFIG := normal
-
-# Use Material sounds.
-$(call inherit-product-if-exists, frameworks/base/data/sounds/AudioPackage13.mk)
-
-$(call inherit-product-if-exists, external/svox/pico/lang/all_pico_languages.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/locales_full.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/generic_no_telephony.mk)
-
-$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony.mk)
-
 # Inherit from those products. Most specific first.
-
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/samsung/codina/codina.mk)
 
 # Discard inherited values and use our own instead.
-
 PRODUCT_NAME := full_codina
 PRODUCT_DEVICE := codina
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_MODEL := GT-I8160
-
