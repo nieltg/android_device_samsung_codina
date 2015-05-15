c318cd9001b7a11e24bc37896f700fc85a922227
Daniel <nieltg@users.noreply.github.com>
2015-05-15 18:28:38 +0700

    Refactor device tree (especially codina.mk)
    NOTE:
    - Move configs/egl.cfg => configs/egl/egl.cfg
    - Move include => configs/ & kernel-headers => configs/
    - Move rootdir/sdboot => configs/root-sdboot & rootdir => configs/root
    - Move configs/* => configs/system/... as specified in old codina.mk
    - Add blob-copier (configs/system => system & configs/root => root)
    - Support sdboot by prepending copier (configs/root-sdboot => root)
    - Move PRODUCT_PROPERTY_OVERRIDES (old codina.mk) to system.prop

--- codina.mk
+++ codina.mk
@@ -26,67 +26,20 @@ PRODUCT_AAPT_PREF_CONFIG := hdpi
 PRODUCT_PACKAGES += \
     NovaThorSettings
 
-# U8500 Common init
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/init.u8500.usb.rc:root/init.u8500.usb.rc
-ifeq ($(TARGET_PRODUCT),cm_codina_sdboot)
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/sdboot/init.u8500.rc:root/init.u8500.rc
-else
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/init.u8500.rc:root/init.u8500.rc
-endif
-
-# Graphics
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.opengles.version=131072 \
-    ro.zygote.disable_gl_preload=1 \
-    ro.bq.gpu_to_cpu_unsupported=1 \
-    debug.sf.hw=1 \
-    debug.hwui.render_dirty_regions=false
-
 # Media
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/omxloaders:system/etc/omxloaders \
-    $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
-    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
     frameworks/av/media/libstagefright/data/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
 # Wifi
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf
 PRODUCT_PACKAGES += \
     libnetcmdiface \
     wpa_supplicant
-PRODUCT_PROPERTY_OVERRIDES += \
-    wifi.interface=wlan0 \
-    wifi.supplicant_scan_interval=150
 $(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4330/device-bcm.mk)
 
-# Bluetooth
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/bluetooth/bt_vendor.conf:system/etc/bluetooth/bt_vendor.conf
-
-# STE
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/cspsa.conf:system/etc/cspsa.conf \
-    $(LOCAL_PATH)/configs/usbid_init.sh:system/bin/usbid_init.sh
-
-# RIL
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.ril.hsxpa=1 \
-    ro.ril.gprsclass=10 \
-    ro.telephony.ril_class=SamsungU8500RIL \
-    ro.telephony.sends_barcount=1 \
-    mobiledata.interfaces=pdp0,wlan0,gprs,ppp0
-
 # Audio
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/configs/asound.conf:system/etc/asound.conf
 PRODUCT_PACKAGES += \
     audio.usb.default \
     audio.a2dp.default \
@@ -102,8 +55,6 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     persist.service.adb.enable=1
 
 # Charger
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/lpm.rc:root/lpm.rc
 PRODUCT_PACKAGES += \
     charger \
     charger_res_images
@@ -124,12 +75,6 @@ PRODUCT_PACKAGES += \
     mkfs.f2fs \
     fsck.f2fs \
     fibmap.f2fs
-
-# Keylayout
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
-    $(LOCAL_PATH)/configs/keylayout/sec_touchkey.kl:system/usr/keylayout/sec_touchkey.kl \
-    $(LOCAL_PATH)/configs/keylayout/simple_remote.kl:system/usr/keylayout/simple_remote.kl
 
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
@@ -162,33 +107,17 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     librs_jni
 
-# Disable error Checking
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.kernel.android.checkjni=0 \
-    dalvik.vm.checkjni=false
-
-# SELinux
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.boot.selinux=permissive
-
-# Storage switch
- PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.vold.switchablepair=sdcard0,sdcard1
-
 # Dalvik VM config for 768MB RAM devices
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.dexopt-data-only=1 \
-    dalvik.vm.heapstartsize=5m \
-    dalvik.vm.heapgrowthlimit=48m \
-    dalvik.vm.heapsize=128m \
-    dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=512k \
-    dalvik.vm.heapmaxfree=4m
 PRODUCT_TAGS += dalvik.gc.type-precise
 
-# KSM
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.ksm.default=1
+# Include blobs
+ifeq ($(TARGET_PRODUCT),cm_codina_sdboot)
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/root-sdboot,root)
+endif
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/system,system) \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/root,root)
 
 # Use the non-open-source parts, if they're present
 include vendor/samsung/u8500-common/vendor-common.mk
@@ -197,31 +126,4 @@ include vendor/samsung/u8500-common/vendor-common.mk
 
 # For better compatibility with ROMs (like Slim, PAC)
 $(call inherit-product, vendor/samsung/u8500-common/codina/codina-vendor-blobs.mk)
-
-# Init files
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/device_tunables.rc:root/device_tunables.rc \
-    $(LOCAL_PATH)/rootdir/init.samsungcodina.rc:root/init.samsungcodina.rc \
-    $(LOCAL_PATH)/rootdir/ueventd.samsungcodina.rc:root/ueventd.samsungcodina.rc
-ifeq ($(TARGET_PRODUCT),cm_codina_sdboot)
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/sdboot/fstab.samsungcodina:root/fstab.samsungcodina \
-    $(LOCAL_PATH)/rootdir/sdboot/init.recovery.samsungcodina.rc:root/init.recovery.samsungcodina.rc
-else
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/fstab.samsungcodina:root/fstab.samsungcodina \
-    $(LOCAL_PATH)/rootdir/init.recovery.samsungcodina.rc:root/init.recovery.samsungcodina.rc
-endif
-
-# STE Modem
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/ste_modem.sh:system/etc/ste_modem.sh
-
-# Audio
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/adm.sqlite-u8500:system/etc/adm.sqlite-u8500
-
-# GPS
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/gps.conf:system/etc/gps.conf
 
